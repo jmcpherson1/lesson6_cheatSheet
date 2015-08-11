14664bece898076bc2120e6e83668550e9952757
Jamasa McPherson <jamasamcpherson@Jamasas-MBP.home>
2015-08-10 23:07:37 -0400

    added Events and Debugging cheats

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+//jQuery Libraries
+	//Inside of the <head> section of the HTML file, include a copy of the jQuery <script>
+
+//EVENTS: When something has been loaded
+	//Load Driven Events
+		.ready()
+
+	//User Driven Events
+		.click()
+		.mouseOver()
+		.mouseOut()
+		.keyUp()
+		.keyDown()
+		.keyPress()
+		
+//Debugging - Use the console to test by calling its functions
+	console.log();
+	alert();
