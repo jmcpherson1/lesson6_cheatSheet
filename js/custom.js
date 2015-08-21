9fcfdf8a62a053b13dd6284a9558f3efe5168288
jmcpherson1 <jamasa.mcpherson@gmail.com>
2015-08-21 03:00:38 -0400

    Update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,21 @@
-$( "#downloadButton" ).click(function() {
-  alert( "Thank you for visiting. Let's stay connected!" );
+$(document).ready(function() {
+
+	$( "#downloadButton" ).click(function() {
+ 		alert( "Thank you for visiting. Let's stay connected!" );
+
+ 	$("#downloadButton").hover(function() {
+        $(this).css("box-shadow", "4px 5px 0px #000");
+    }, function() {
+        $(this).css("box-shadow", "0px 0px 0px #000");
+    });
+
+     $("#downloadButton").hover(function() {
+        $(this).css("color", "gray ");
+    }, function() {
+        $(this).css("color", "black");
+    });
+
+
 });
 
+});
